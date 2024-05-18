f3241fe8b636dc0d4c945213dcd837d431ee2fef
Fokin Maxim <fokinmu@refactorx.ru>
2024-05-18 21:04:10 +0700

    Add migrations

--- Coworking.Backend/Coworking.Core.DA/Migrations/20240518135602_AddVectorConverterResult.Designer.cs
+++ Coworking.Backend/Coworking.Core.DA/Migrations/20240518135602_AddVectorConverterResult.Designer.cs
@@ -2,6 +2,7 @@
 using Coworking.Core.DA;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Coworking.Core.DA.Migrations
 {
     [DbContext(typeof(CoworkingDbContext))]
-    partial class CoworkingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240518135602_AddVectorConverterResult")]
+    partial class AddVectorConverterResult
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Coworking.Backend/Coworking.Core.DA/Migrations/20240518135602_AddVectorConverterResult.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Coworking.Core.DA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVectorConverterResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "FileConverterResults",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FloorId = table.Column<int>(type: "integer", nullable: false),
+                    FloorLayoutContent = table.Column<string>(type: "text", nullable: true),
+                    ContentType = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileConverterResults", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileConverterResults",
+                schema: "public");
+        }
+    }
+}
